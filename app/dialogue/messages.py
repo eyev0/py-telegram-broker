3f2273ef1be3ea6321f96e4324105dc0ef8cbb72
Egor Dementyev <bespamiatny@gmail.com>
2020-06-03 21:49:31 +0300

    messages.py

--- app/dialogue/messages.py
+++ app/dialogue/messages.py
@@ -1,20 +1,21 @@
-greetings = 'Yo, давай знакомиться. Для начала напиши, в каком городе ты живешь, ' \
+greetings = 'Давай знакомиться. Для начала напиши, в каком городе ты живешь, ' \
             'чтобы я мог зарегистрировать тебя в базе пользователей нашей площадки.'
-sign_up_complete = 'Yo, отлично! Можешь заливать картон.'
-yo = 'Yo, как дела?'
+sign_up_complete = 'Отлично! Можешь заливать картон.'
+yo = 'Как дела?'
 help_ = ' '
 
+user_inactive = 'Ты в инактиве, друг! Обратись в саппорт.'
+upload_parse_failed = 'Что-то не так с форматом твоего списка.. Не могу распарсить твой запрос на добавление'
+upload_limit_exceeded = 'Твой лимит объявлений({}) превышен! Нельзя загрузить {} карт'
 
-upload = 'Yo, скинь мне список карт, которые хочешь загрузить. ' \
+upload = 'Скинь мне список карт, которые хочешь загрузить. ' \
                         'Каждая строка должна иметь формат <Название>,<Цена>\n' \
                         'Например, так:\n\n' \
                         'Серый выхухоль,10\n' \
                         'Черная вдова,25\n' \
                         'Скрытный оползень,100\n' \
                         '...'
-upload_code_inactive = 'Yo, ты в инактиве, друг! Ты не можешь загружать ничего, обратись в саппорт.'
-upload_code_limit = 'Yo, твой лимит объявлений({}) превышен! Нельзя загрузить {} карт'
-upload_complete = 'Yo, Успех!'
+upload_complete = 'Успех!'
 
 admin_enable = 'Ладно... будешь за админа теперь!'
 admin_disable = 'Теперь ты как все, друг!'
@@ -31,9 +32,16 @@ MESSAGES = {
     'admin_enable': admin_enable,
     'admin_disable': admin_disable,
 
-    # create event messages
+    # errors
+    'user_inactive': user_inactive,
+    'upload_parse_failed': upload_parse_failed,
+    'upload_limit_exceeded': upload_limit_exceeded,
+
+    # upload messages
     'upload': upload,
-    'upload_code_inactive': upload_code_inactive,
-    'upload_code_limit': upload_code_limit,
     'upload_complete': upload_complete,
 }
+
+# YO!
+for key in MESSAGES:
+    MESSAGES[key] = 'Yo, ' + MESSAGES[key]
