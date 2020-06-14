928c63542388262a43f1f04f374cffd88b17ff60
Egor Dementyev <bespamiatny@gmail.com>
2020-06-14 16:10:05 +0300

    test pre-commit hooks

--- app/__init__.py
+++ app/__init__.py
@@ -10,28 +10,31 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-from app.config import Config, ConfigManager
+from app.config import ConfigManager
 
-clock = datetime(2020, 1, 1, tzinfo=pytz.timezone('Europe/Moscow'))
+clock = datetime(2020, 1, 1, tzinfo=pytz.timezone("Europe/Moscow"))
 
-debug = bool(os.getenv('PY_APP_DEBUG_MODE', 'False'))
+debug = bool(os.getenv("PY_APP_DEBUG_MODE", "False"))
 config = ConfigManager(debug).config
 
 stdout_handler = logging.StreamHandler(sys.stderr)
 log_handlers = [stdout_handler]
 if config.log.use_file:
     try:
-        with open(config.log.file_path, mode='x'):
+        with open(config.log.file_path, mode="x"):
             pass
     except FileExistsError:
         pass
     file_handler = logging.FileHandler(config.log.file_path)
     log_handlers.append(file_handler)
 # noinspection PyArgumentList
-logging.basicConfig(format=u'%(filename)s [ LINE:%(lineno)+3s ]#%(levelname)+8s [%(asctime)s]  %(message)s',
-                    level=config.log.level,
-                    handlers=log_handlers)
-logging.log(config.log.level, f'\n{config!r}')
+logging.basicConfig(
+    format="%(filename)s [ LINE:%(lineno)+3s ]"
+    "#%(levelname)+8s [%(asctime)s]  %(message)s",
+    level=config.log.level,
+    handlers=log_handlers,
+)
+logging.log(config.log.level, f"\n{config!r}")
 
 event_loop = asyncio.get_event_loop()
 proxy_url = None
@@ -39,16 +42,23 @@ proxy_auth = None
 if config.app.use_proxy:
     proxy_url = config.proxy.url
     if len(config.proxy.username) > 0:
-        proxy_auth = aiohttp.BasicAuth(login=config.proxy.username,
-                                       password=config.proxy.password)
-bot = Bot(token=config.app.token,
-          loop=event_loop,
-          proxy=proxy_url,
-          proxy_auth=proxy_auth)
-dp = Dispatcher(bot,
-                loop=event_loop,
-                storage=RedisStorage2(config.redis.host,
-                                      config.redis.port,
-                                      db=config.redis.db,
-                                      prefix=config.redis.prefix))
+        proxy_auth = aiohttp.BasicAuth(
+            login=config.proxy.username, password=config.proxy.password
+        )
+bot = Bot(
+    token=config.app.token,
+    loop=event_loop,
+    proxy=proxy_url,
+    proxy_auth=proxy_auth,
+)
+dp = Dispatcher(
+    bot,
+    loop=event_loop,
+    storage=RedisStorage2(
+        config.redis.host,
+        config.redis.port,
+        db=config.redis.db,
+        prefix=config.redis.prefix,
+    ),
+)
 dp.middleware.setup(LoggingMiddleware())
