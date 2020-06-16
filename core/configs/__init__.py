f16147643269deccee0afd56521a47d03fd10519
Egor Dementyev <bespamiatny@gmail.com>
2020-06-16 16:28:25 +0300

    breaking: remove load_dotenv, now load .env manually

--- core/configs/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from dotenv import find_dotenv, load_dotenv
-
-DOCKER_COMPOSE = os.getenv("DOCKER_COMPOSE", "false").lower() in ["true", "1", "yes"]
-if not DOCKER_COMPOSE:
-    load_dotenv(find_dotenv(), verbose=True)
