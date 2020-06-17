65c31678991dea2e8938fdfd09561093cba33ebf
Egor Dementyev <bespamiatny@gmail.com>
2020-06-17 13:07:39 +0300

    fix: add default values to env vars

--- core/configs/proxy.py
+++ core/configs/proxy.py
@@ -1,6 +1,6 @@
 import os
 
-PROXY_USE = os.getenv("PROXY_USE", "false").lower() in ["true", "1", "yes"]
+PROXY_USE = os.getenv("PROXY_USE", "False").lower() in ["true", "1", "yes"]
 PROXY_URL = os.getenv("PROXY_URL", "socks5://0.0.0.0:1080")
 PROXY_USERNAME = os.getenv("PROXY_USERNAME", "")
 PROXY_PASSWORD = os.getenv("PROXY_PASSWORD", "")
--- core/configs/telegram.py
+++ core/configs/telegram.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 BOT_SU = os.getenv("BOT_SU", "").split(",")
 BOT_ADMINS = []
