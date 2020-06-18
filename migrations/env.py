6d3d9bf0390e961844f31969ce33f9341a0d23b1
Egor Dementyev <bespamiatny@gmail.com>
2020-06-18 13:45:57 +0300

    feat: pass POSTGRES_URI var to alembic, rename migrations folder

--- migrations/env.py
+++ migrations/env.py
@@ -1,24 +1,15 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from alembic import context
-from core.database.models import Base
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from app.config import POSTGRES_URI
+from app.database.models import Base
 
 config = context.config
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", POSTGRES_URI)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
